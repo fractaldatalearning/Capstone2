cdac38a20bb6c267ba6660bee78cf5e1b4fdfb71
fractaldatalearning <104742545+fractaldatalearning@users.noreply.github.com>
2022-09-18 16:11:04 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='all resources for dsc capstone 2 for KL',
+    author='Katin',
+    license='BSD-3',
+)
